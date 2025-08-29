f1dea649738337770b9f2e7d2f0548f417f92727
bekdev09 <elbekmovlonov@gmail.com>
2025-08-29 16:45:11 +0500

    added package

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express, { type NextFunction, type Request, type Response } from "express
 import cors from "cors";
 import morgan from "morgan"
 import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(cors())
 app.use(helmet())
+app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 100 }));
+
 app.use(express.json());
 app.use(morgan('dev'))
 
