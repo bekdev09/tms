a0bedcda0caafe68488876f9ed7f0e13cc6551bc
bekdev09 <elbekmovlonov@gmail.com>
2025-09-17 16:34:28 +0500

    fixed: global error handler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,13 @@
 import express, { Application } from "express";
-
 import { setupSecurity } from "./middlewares/security.ts";
 import mainRouter from "./routes/index.ts";
+import { errorHandlerMiddleware } from "./middlewares/errorHandler.ts";
 
 const app: Application = express();
 
 setupSecurity(app);
 
 app.use("/api/v1", mainRouter);
+app.use(errorHandlerMiddleware);
 
 export default app
---
+++ server/src/middlewares/errorHandler.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+import { Prisma } from "@prisma/client";
+import { StatusCodes } from "http-status-codes";
+
+interface CustomError {
+  statusCode: number;
+  msg: string;
+}
+
+export function errorHandlerMiddleware(
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  console.error("🔥 Error:", err);
+
+  let customError: CustomError = {
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: "Something went wrong, please try again later",
+  };
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    customError.msg = err.message;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    switch (err.code) {
+      case "P2002":
+        customError.msg = `Duplicate value entered for field: ${err.meta?.target}`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+        break;
+
+      case "P2003": // Foreign key constraint failed
+        customError.msg = `Invalid relation: ${err.meta?.field_name}`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+        break;
+
+      case "P2025":
+        customError.msg = "Record not found";
+        customError.statusCode = StatusCodes.NOT_FOUND;
+        break;
+
+      default:
+        customError.msg = `Database error: ${err.message}`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+}
